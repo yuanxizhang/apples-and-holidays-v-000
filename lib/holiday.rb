f550658a6579f5817438005c573edc1ad7ef6978
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-07 06:54:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   
+  holiday_hash[season][holiday_name] =  
   # remember to return the updated hash
 
 end
