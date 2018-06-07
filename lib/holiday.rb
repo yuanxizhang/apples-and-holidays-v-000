aeb456dfb458e91e45344ea3b47c8b97d5337b75
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-07 06:56:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.inject(:+)
 end
 
 def all_supplies_in_holidays(holiday_hash)
