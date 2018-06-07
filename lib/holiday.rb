a4c9a11c3e5a4920d4c8ba7fe76e7ea91c2cf391
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-07 07:39:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holidays = []
+  holiday_hash.each do |ss, hh|
+     
+    hh.each do |holiday, list|
+       
+    end
+  end
 
 end
 
